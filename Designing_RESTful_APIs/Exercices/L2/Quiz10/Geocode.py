4261c2bbe11d0ddec8371887fa47903cf273b5a0
NaPiZip <napizip@mail.com>
2019-02-20 22:33:51 -0500

    Minor updates, added requests and flasks to requirements.txt.

--- Designing_RESTful_APIs/Exercices/L2/Quiz10/Geocode.py
+++ Designing_RESTful_APIs/Exercices/L2/Quiz10/Geocode.py
@@ -2,16 +2,16 @@
 Geocode.py
 
 
-This module is used to find a Geocode locataion based on a provided input string. The module is using the MapQuest API in order to query the location, for details see https://developer.mapquest.com/documentation/ .  
+This module is used to find a Geocode locataion based on a provided input string. The module is using the MapQuest API in order to query the location, for details see https://developer.mapquest.com/documentation/ .
 
-Example:<br> 
+Example:<br>
         obj = GeocodeLocation('api_key')
         coordinates = obj.getGeocodeLocation('Tokyo, Japan')
 
 The return value is a dictionary of latitued and longitude coordinates.
-    
+
         {'lat': 35.680071, 'lng': 139.768522}
-    
+
 
 Todo:
     * Implementing a proper unit test as well as a object based implementation of the function.
@@ -26,7 +26,7 @@ class GeocodeLocation(object):
                    ERROR_WITH_INPUT = 400,
                    KEY_RELATED_ERROR = 403,
                    UNKNOWN_ERROR = 500)
-    
+
     def __init__(self, key = ''):
         self.mapquest_key = key
 
@@ -44,30 +44,30 @@ class GeocodeLocation(object):
             if(response_dict.get('info').get('statuscode') == self.status_code['SUCESS']):
                return (response_dict.get('results')[0].get('locations')[0].get('latLng'))
             else:
-               return[]        
+               return[]
         else:
-            return[]  
-        
+            return[]
+
 class TestStringMethods(unittest.TestCase):
-    
+
     def setUp(self):
         self.valid_key = 'MY_KEY'
-    
+
     def test_empty_key_and_imput(self):
-        obj = GeocodeLocation()        
+        obj = GeocodeLocation()
         self.assertEqual(obj.getGeocodeLocation(''),[])
-        
+
     def test_empty_input(self):
-        obj = GeocodeLocation(self.valid_key)        
+        obj = GeocodeLocation(self.valid_key)
         self.assertEqual(obj.getGeocodeLocation(''),[])
-        
+
     def test_empty_key(self):
-        obj = GeocodeLocation('')        
+        obj = GeocodeLocation('')
         self.assertEqual(obj.getGeocodeLocation('Spain'),[])
-               
+
     def test_valid_location(self):
         obj = GeocodeLocation(self.valid_key)
         self.assertEqual(obj.getGeocodeLocation('Tokyo, Japan'),{'lat': 35.680071, 'lng': 139.768522})
-        
-if __name__ == '__main__':   
+
+if __name__ == '__main__':
     unittest.main(argv=[''], verbosity=1, exit=False)

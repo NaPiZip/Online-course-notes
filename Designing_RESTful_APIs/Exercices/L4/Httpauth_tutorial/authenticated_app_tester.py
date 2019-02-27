12bcf903dcff48f6b7be1dbb1f7e2d7e3afcfc9c
NaPiZip <napizip@mail.com>
2019-02-26 22:52:31 -0500

    Added proper string format.

--- Designing_RESTful_APIs/Exercices/L4/Httpauth_tutorial/authenticated_app_tester.py
+++ Designing_RESTful_APIs/Exercices/L4/Httpauth_tutorial/authenticated_app_tester.py
@@ -9,6 +9,6 @@ import json
 
 if __name__ == '__main__':
     h = httplib2.Http()
-    h.add_credentials(r"Nawin",r"1234")
+    h.add_credentials(b"Nawin",b"1234")
     resp = h.request('http://localhost:5000/','GET')
     print(resp)

1911fb69037ed44e3410c1718a0f1e5384dfb18a
NaPiZip <napizip@mail.com>
2019-02-28 21:08:32 -0500

    Added test.

--- Designing_RESTful_APIs/Exercices/L4/Httpauth_tutorial/authenticated_app_tester.py
+++ Designing_RESTful_APIs/Exercices/L4/Httpauth_tutorial/authenticated_app_tester.py
@@ -2,5 +2,11 @@ from requests.auth import HTTPDigestAuth
 import requests
 
 if __name__ == '__main__':
+
+     print ("Making a GET request to authenticated /...")
      url = 'http://localhost:5000/'
-     requests.get(url, auth=HTTPDigestAuth('Time', '1234'))
+     resp = requests.get(url, auth=HTTPDigestAuth('Time', '1234'))
+     if resp.status_code == requests.codes.ok:
+         print("Test PASSED!")
+     else:
+        print("Test FAILED!")

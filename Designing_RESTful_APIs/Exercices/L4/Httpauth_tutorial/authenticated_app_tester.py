197c8c6ce422cfabed06330b916a208951faed8f
NaPiZip <napizip@mail.com>
2019-03-02 17:54:18 -0500

    Fixed indent.

--- Designing_RESTful_APIs/Exercices/L4/Httpauth_tutorial/authenticated_app_tester.py
+++ Designing_RESTful_APIs/Exercices/L4/Httpauth_tutorial/authenticated_app_tester.py
@@ -1,12 +1,13 @@
 from requests.auth import HTTPDigestAuth
 import requests
+import httplib2
 
 if __name__ == '__main__':
+    print ("Making a GET request to authenticated /...")
+    url = 'http://localhost:5000/'
+    resp = requests.get(url, auth=HTTPDigestAuth('Time', '1234'))
 
-     print ("Making a GET request to authenticated /...")
-     url = 'http://localhost:5000/'
-     resp = requests.get(url, auth=HTTPDigestAuth('Time', '1234'))
-     if resp.status_code == requests.codes.ok:
-         print("Test PASSED!")
-     else:
+    if resp.status_code == requests.codes.ok:
+        print("Test PASSED!")
+    else:
         print("Test FAILED!")

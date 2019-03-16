c4780f4954d7b3106048498a510b5a098b1a51a9
NaPiZip <napizip@mail.com>
2019-03-15 23:43:17 -0400

    Added starter code.

---
+++ Designing_RESTful_APIs/Exercices/L4/Quiz14/models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declaritive import declaritive_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+from passlib.apps import custom_app_context as pwd_context
+
+Base = declaritive_base()
+
+class Item(Base):
+    __tablename__ = 'item'
+    id          = Column(Integer, primary_key = True)
+    name        = Column(String)
+    picture     = Column(String)
+    description = Column(String)
+    price       = Column(String)
+
+    @property
+    def serialize(self):
+        return dict(
+        name        = self.name,
+        picture     = self.picture,
+        price       = self.price,
+        description = self.description
+        )
+
+engine = create_engine('sqlite:///barginMart.db/?check_same_thread=False', echo = True')
+
+Base.metadata.create_all(engine)

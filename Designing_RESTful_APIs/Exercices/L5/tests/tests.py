4b4ce3f58f2a23c8d0da7fc3c5b20bba8866fd8f
NaPiZip <napizip@mail.com>
2019-04-04 22:23:33 -0400

    Added test case for valid and invalid login.

--- Designing_RESTful_APIs/Exercices/L5/tests/tests.py
+++ Designing_RESTful_APIs/Exercices/L5/tests/tests.py
@@ -13,34 +13,42 @@ from dbSession import session as _session, engine
 
 class TestCase(unittest.TestCase):
     clean_up    = True
-    db_name     = 'finalProject.db'
-    db_url      = 'sqlite:///' + db_name
     session     = _session
     app = []
     def setUp(self):
         app.config['TESTING'] = True
         app.secret_key="Something"
-        #app.config['SQLALCHEMY_DATABASE_URI'] = self.db_url
         self.app = app.test_client()
         login_manager.init_app(app)
         login_manager.login_view = 'login_api.login'
 
     def tearDown(self):
         if self.clean_up:
-            print("Cleaning {}".format(self.db_name))
             self.session.close()
+
+    def tearDownClass():
+        os.remove('finalProject.db')
 
     def does_url_response_contain_substring(self ,url, sub_string):
         response = self.app.get(url, follow_redirects=True)
         self.assertEqual(response.status_code,200)
+        self.assertTrue(self.does_data_contain_substring(response.data, sub_string))
 
-        data = str(response.data)
+    def does_data_contain_substring(self, in_data, sub_string):
+        data = str(in_data)
         if sub_string is not str:
             sub_string = str(sub_string)
         if sub_string in data:
-            return
+            return True
         else:
-            self.assertTrue(False)
+            return False
+
+    def create_minimal_user(self, user_name, password):
+        new_user = User(user_name=user_name, email='something@google.com')
+        new_user.generate_api_key()
+        new_user.hash_password(password)
+        self.session.add(new_user)
+        self.session.commit()
 
     def test_get_login_route(self):
         self.does_url_response_contain_substring( '/login', r'<title>Login</title>')
@@ -54,16 +62,22 @@ class TestCase(unittest.TestCase):
     def test_get_logout_page_whitout_logged_in_user(self):
         self.does_url_response_contain_substring( '/logout', r'<title>Login</title>')
 
-
     def test_post_request_with_wrong_password(self):
-        new_user = User(user_name='Nawin', email='something@google.com')
-        new_user.generate_api_key()
-        new_user.hash_password('1234')
-        self.session.add(new_user)
-        self.session.commit()
+        self.create_minimal_user('Tim','1234')
+        data = dict(username='Tim', password='qweq')
+        response = self.app.post('/login', data=data, follow_redirects=True)
+        self.assertEqual(response.status_code,200)
+        self.assertTrue(self.does_data_contain_substring(response.data,r'<strong>Error!</strong> Invalid Credentials\n'))
+
+    def test_post_request_with_correct_password(self):
+        self.create_minimal_user('Nawin','1234')
         data = dict(username='Nawin', password='1234')
-        #response = self.app.post('/login',data=data)
-        #self.assertEqual(response.status_code,200)
+        response = self.app.post('/login', data=data, follow_redirects=True)
+        self.assertEqual(response.status_code,200)
+        if response.is_json:
+            self.assertNotEqual(response.get_json().get('api_key'),None)
+        else:
+            self.assertTrue(False)
 
 
 if __name__ == '__main__':

44baac4b709f6b5e510aceb92e532740c2418051
NaPiZip <napizip@mail.com>
2019-04-21 22:18:54 -0400

    Refactored /v1/requests post route.

--- Designing_RESTful_APIs/Exercices/L5/appEndpoints.py
+++ Designing_RESTful_APIs/Exercices/L5/appEndpoints.py
@@ -64,35 +64,6 @@ def get_user_with_id(id):
         return jsonify(dict(message="ERROR, user {} not found!".format(id))),404
 
 
-@app_endpoints.route('/v1/requests', methods = ['GET', 'POST'])
-@require_api_key
-def show_make_user_requests():
-    if request.method == 'GET':
-        meal_requests = session.query(MealRequest).all()
-        if meal_requests is not None:
-            return jsonify( [req.serialize for req in meal_requests])
-        else:
-            return 'None'
-    elif request.method == 'POST':
-        try:
-            new_meal_request = MealRequest(**request.json,user_id=current_user.id)
-            response = requests.get('https://api.foursquare.com/v2/venues/search',params={**foursquare_key_dict, 'v':'20180323', 'limit':1,
-                            'near':request.json.get('location_area'),
-                            'query':request.json.get('meal_type')})
-            if response.status_code != 200:
-                return jsonify(dict(message="ERROR, foursquare api not working {}!".format(status_code))),404
-
-            new_meal_request.location_name = response.json().get('response').get('venues')[0].get('name')
-            new_meal_request.latitude = response.json().get('response').get('geocode').get('feature').get('geometry').get('center').get('lat')
-            new_meal_request.longitude =  response.json().get('response').get('geocode').get('feature').get('geometry').get('center').get('lng')
-            current_user.meal_requests.append(new_meal_request)
-            session.commit()
-            return jsonify(dict(message="Success, created request: {}!".format(new_meal_request.id))),201
-        except ValueError as value_error:
-            return jsonify(dict(message=value_error.args)),404
-        return 'None'
-
-
 def update_meal_request(MealRequest):
     response = requests.get('https://api.foursquare.com/v2/venues/search',params={**foursquare_key_dict, 'v':'20180323', 'limit':1,
                     'near':MealRequest.location_area,
@@ -105,6 +76,32 @@ def update_meal_request(MealRequest):
     MealRequest.longitude =  response.json().get('response').get('geocode').get('feature').get('geometry').get('center').get('lng')
 
     return MealRequest,response.status_code
+
+
+@app_endpoints.route('/v1/requests', methods = ['GET', 'POST'])
+@require_api_key
+def show_make_user_requests():
+    if request.method == 'GET':
+        meal_requests = session.query(MealRequest).all()
+        if meal_requests is not None:
+            return jsonify( [req.serialize for req in meal_requests])
+        else:
+            return 'None'
+    elif request.method == 'POST':
+        try:
+            new_meal_request = MealRequest(**request.json,user_id=current_user.id)
+
+            new_meal_request,status_code = update_meal_request(new_meal_request)
+
+            if status_code == 200:
+                current_user.meal_requests.append(new_meal_request)
+                session.commit()
+                return jsonify(dict(message="Success, created request: {}!".format(new_meal_request.id))),201
+            else:
+                return jsonify(dict(message="ERROR, foursquare api not working {}!".format(status_code))),404
+
+        except ValueError as value_error:
+            return jsonify(dict(message=value_error.args)),404
 
 @app_endpoints.route('/v1/requests/<int:id>', methods = ['GET', 'PUT', 'DELETE'])
 @require_api_key

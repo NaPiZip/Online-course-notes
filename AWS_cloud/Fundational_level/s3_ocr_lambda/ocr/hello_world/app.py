0bc0b67c5596e3952a42ee4562b2bfa91d6c4df4
NaPiZip <napizip@mail.com>
2020-05-16 15:55:16 +0200

    Saved current working progress after successfully getting the tesseract elf to run in sam local.

---
+++ AWS_cloud/Fundational_level/s3_ocr_lambda/ocr/hello_world/app.py
@@ -0,0 +1,61 @@
+import os
+import json
+import subprocess
+
+# import requests
+
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+LIB_DIR = os.path.join(SCRIPT_DIR, 'lib')
+
+def lambda_handler(event, context):
+    """Sample pure Lambda function
+
+    Parameters
+    ----------
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
+
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
+    API Gateway Lambda Proxy Output Format: dict
+
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """
+
+
+    image_file_path = 'example.png'
+    language = 'deu'
+    
+    command = 'LD_LIBRARY_PATH={} TESSDATA_PREFIX={} ./tesseract {} stdout -l {}'.format(
+            LIB_DIR,
+            SCRIPT_DIR+'/tessdata',
+            image_file_path,
+            language
+        )
+
+    
+    print(command)    
+  
+    try:              
+        output = subprocess.check_output(command, shell=True) 
+        result = str(output,'utf-8')
+    except :
+        result = "Failed with: {}".format(output)
+        pass
+
+    print("\n",result)
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "result": result
+        }),
+    }
